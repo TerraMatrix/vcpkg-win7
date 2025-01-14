7c4d78de91518d6cc0dff3090c696a7a76a41a4e
FangYi <fywhu@qq.com>
2025-01-15 02:50:48 +0800

    [flann] flann版本固定到2019-04-07#7

--- BuildAll.bat
+++ BuildAll.bat
@@ -27,3 +27,7 @@ call ./bash/freetype.bat
 echo "install  hdf5 1.14.4.3#3"
 echo "--------------------------------------------"
 call ./bash/hdf5.bat
+
+echo "install  flann 2019-04-07#7"
+echo "--------------------------------------------"
+call ./bash/flann.bat
